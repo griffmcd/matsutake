923f91195f7cdb5d57c95601ded93132b0556d63
Griffin McDonald <griffin.mcdonald@pipe-flo.com>
2020-11-09 15:37:51 -0800

    added basic vie3w stuff

---
+++ src/public/scripts/login.js
@@ -0,0 +1,15 @@
+document.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (event.target.matches('#login-btn')) {
+        var emailInput = document.getElementById('email-input');
+        var pwdInput = document.getElementById('pwd-input');
+        var data = {
+            username: emailInput.value,
+            password: pwdInput.value
+        };
+        Http.Post('/api/auth/login', data)
+            .then(() => {
+                window.location.href = '/users';
+            })
+    }
+}, false)
