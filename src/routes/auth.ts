0b9b8d0784233aaba0d259c21a273fcc163132de
Griffin McDonald <griffin.mcdonald@pipe-flo.com>
2020-11-09 15:37:08 -0800

    added comments detailing exact routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,10 +3,10 @@ import AuthController from '../controllers/AuthController';
 import checkJwt from '../middlewares/checkJwt';
 
 const router = Router();
-// login route
+// login user - POST /api/auth/login
 router.post('/login', AuthController.login);
 
-// change password
+// change password - POST /api/auth/change-password
 router.post('/change-password', [checkJwt], AuthController.changePassword);
 
 export default router;
