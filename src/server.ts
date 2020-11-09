0a433ad728a7ad447a1de7e4906f4e21a1b00cb3
Griffin McDonald <griffin.mcdonald@pipe-flo.com>
2020-11-09 15:39:44 -0800

    refactored the router api, and added view support

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,22 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import express, { NextFunction, Request, Response } from 'express';
 import StatusCodes from 'http-status-codes';
-import routes from './routes';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import BaseRouter from './routes';
 import logger from './shared/logger';
+import config from './config/config';
 
 const app = express();
 const { BAD_REQUEST } = StatusCodes;
 
-// basic express settings
+/*
+ * basic express settings
+ */
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(config.cookieProps.secret));
 
 // show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
@@ -26,8 +33,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(bodyParser.json());
 // add apis
-// TODO: look at BaseRouter style routing in cheri-pi project
-app.use('/', routes);
+app.use('/api', BaseRouter);
 
 // Print API errors
 // eslint-disable-next-line no-unused-vars
@@ -37,5 +43,36 @@ app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
     error: err.message,
   });
 });
+
+/**
+ * serve front end content
+ */
+
+const viewsDir = path.join(__dirname, 'views');
+app.set('views', viewsDir);
+const staticDir = path.join(__dirname, 'public');
+app.use(express.static(staticDir));
+
+app.get('/', (req: Request, res: Response) => {
+  res.sendFile('login.html', { root: viewsDir });
+});
+
+app.get('/users', (req: Request, res: Response) => {
+  const { signedCookies } = req;
+  logger.info('f');
+  if (signedCookies === undefined) {
+    res.redirect('/');
+  }
+  const jwt = req.signedCookies[config.cookieProps.key];
+  if (!jwt) {
+    res.redirect('/');
+  } else {
+    res.sendFile('users.html', { root: viewsDir });
+  }
+});
+
+/**
+ * export server
+ */
 
 export default app;
