df50a8eae1a78a0eff1ab2c201765cd155817559
Griffin McDonald <griffin.mcdonald@pipe-flo.com>
2020-11-08 23:30:00 -0800

    refactored how index works a little

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,28 @@
 import './preStart'; // must be first import
 import 'reflect-metadata';
-import { createConnection } from 'typeorm';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as helmet from 'helmet';
 import * as cors from 'cors';
 import routes from './routes';
+import connectDB from './database';
 
-createConnection()
-  // eslint-disable-next-line no-unused-vars
-  .then(async (_connection) => {
-    // create express app
-    const app = express();
+const startServer = () => {
+  // create express app
+  const app = express();
 
-    // middlewares
-    app.use(cors());
-    app.use(helmet());
-    app.use(bodyParser.json());
+  // middlewares
+  app.use(cors());
+  app.use(helmet());
+  app.use(bodyParser.json());
 
-    app.use('/', routes);
-
-    app.listen(3000, () => {
-      // eslint-disable-next-line no-console
-      console.log('Server started on port 3000!');
-    });
-  })
-  // eslint-disable-next-line no-console
-  .catch((error) => console.log(error));
+  app.use('/', routes);
+  app.listen(3000, () => {
+    // eslint-disable-next-line no-console
+    console.log('Server started on port 3000!');
+  });
+};
+(async () => {
+  await connectDB();
+  startServer();
+})();
