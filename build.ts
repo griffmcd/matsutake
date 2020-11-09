96e1f3ac813c3169f7db62354b5e17d6f9a212b4
Griffin McDonald <griffin.mcdonald@pipe-flo.com>
2020-11-09 15:38:20 -0800

    added the cleanup stage to the pre build script

--- build.ts
+++ build.ts
@@ -1,14 +1,13 @@
 // remove old files, copy front end ones
 
 import fs from 'fs-extra';
-// import logger
 
 try {
   // remove current build
   fs.removeSync('./dist/');
   // copy front end files
-  // fs.copySync('./src/public', './dist/public');
-  // fs.copySync('./src/views', './dist/views');
+  fs.copySync('./src/public', './dist/public');
+  fs.copySync('./src/views', './dist/views');
 } catch (err) {
   console.log(err);
 }
