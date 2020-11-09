6a5cd61b610e7bf6aceaa6a5e2559fd99344a545
Griffin McDonald <griffin.mcdonald@pipe-flo.com>
2020-11-09 15:39:55 -0800

    added configuration for cookie properties

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,16 @@
 export default {
+  cookieProps: Object.freeze({
+    key: 'ExpressGeneratorTs',
+    secret: process.env.COOKIE_SECRET,
+    options: {
+      httpOnly: true,
+      signed: true,
+      path: (process.env.COOKIE_PATH),
+      maxAge: Number(process.env.COOKIE_EXP),
+      domain: (process.env.COOKIE_DOMAIN),
+      secure: (process.env.SECURE_COOKIE === 'true'),
+    },
+  }),
   jwtSecret: process.env.JWT_SECRET || '',
   dirName: process.env.DIR_NAME || './src',
   ext: process.env.EXT || '.ts',
